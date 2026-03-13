e6aa494cee7e162fd39e62e69f0d650e3a4c53ba
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2026-03-13 09:46:09 -0300

    exemplos
    do 06 a 08

---
+++ exemplo06.php
@@ -0,0 +1,13 @@
+<?php
+
+class conta 
+{
+    public $numero;
+    public $saldo;
+
+    function __construct($numero,$saldo )
+    {
+        $this->numero = $numero;
+        $this->saldo = $saldo;
+    }
+}
