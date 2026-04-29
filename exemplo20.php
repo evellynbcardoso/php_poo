49e11abdc623079b30cd3babe69b3c842f9a87ee
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2026-04-29 16:44:04 -0300

    exemplo20

--- exemplo20.php
+++ exemplo20.php
@@ -7,13 +7,13 @@ interface INotificador{
 // Implementar : e-mail
 class NotificadorEmail implements   INotificar{
     public function enviar ($destinatario, $mensagem){
-        echo "Email enviado para {$destinatario}. Mensagem: {$mensagem}";
+        echo "Email enviado para {$destinatario}. Mensagem: {$mensagem}.";
     }
 }
 
 // Implementar:SMS
 class NotificadorSMS implements INotificador{
-    public function enviar($|destinatario, $mensagem){
+    public function enviar($destinatario, $mensagem){
         echo "SMS enviar {$destinatario}. Mensagem: {$mensagem}.";
     }
 }
@@ -24,3 +24,24 @@ class NotificadorWhatsapp implements INotificador{
         echo "Whatsapp enviado para {$}"
     }
 }
+
+// Classe que usa a interface
+class SistemaDeNotificacoes{
+    private $notificador;
+
+    public function __construct($notificador $notificador){
+        $this->notificador = $notificador;
+    }
+
+    public function notificarUsuario($destinatario, $mensagem){
+        $this->notificador->enviar($destinatario, $mensagem);
+    }
+}
+
+$sistemaEmail = new SistemaDeNotificacoe(new NotificadorEmail);
+$sistemaSMS = new SistemaDeNotificacoe(new NotificadorSMS);
+$sistemaWhatsapp = new SistemaDeNotificacoe(new NotificadorWhatsapp);
+
+$sistemaEmail->notificarUsuario("joão@email.com", "Seu pedido foi confirmado");
+$sistemaSMS->notificarUsuario("17997651234", "Seu pedido foi confirmado");
+$sistemaEmail->notificarUsuario("17997651234", "Seu pedido foi confirmado");
