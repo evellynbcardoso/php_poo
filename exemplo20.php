8ae4fb8f4a2df195914b5019c717bb89feac02fb
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2026-04-29 15:17:34 -0300

    exemplo20

---
+++ exemplo20.php
@@ -0,0 +1,19 @@
+<?php
+
+interface INotificador{
+    public function enviar($destinatario, $mensagem);
+}
+
+// Implementar : e-mail
+class NotificadorEmail implements   INotificar{
+    public function enviar ($destinatario, $mensagem){
+        echo "Email enviado para {$destinatario}. Mensagem: {$mensagem}";
+    }
+}
+
+// Implementar:SMS
+class NotificadorSMS implements INotificador{
+    public function enviar($|destinatario, $mensagem){
+        echo "SMS enviar {$destinatario}. Mensagem: {$mensagem}.";
+    }
+}
