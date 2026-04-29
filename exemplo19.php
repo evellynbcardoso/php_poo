b7eec01048c1d5c7eefa32b3f64b46f992773370
Evellyn <evellyn.b.cardoso@aluno.senai.br>
2026-04-29 14:19:16 -0300

    pronto

--- exemplo19.php
+++ exemplo19.php
@@ -33,3 +33,28 @@ class Circunferencia extends FiguraGeometrica{
         return 2 * 3.14 * $this->raio;
     }
 }
+
+class Retangulo extends FiguraGeometrica{
+    private $lado1;
+    private $lado2
+
+    public function ___contruct($tipo, $lado1, $lado2){
+        parent::__contruct($tipo);
+        $this->lado1 = $lado1;
+        $this->lado2 = $lado2;
+    }
+
+    public function area(){
+        return $this->lado1 * $this->lado2;
+    }
+
+    public function perimetro(){
+        return (2 * $this->lado1) + (2 * $this->lado2);
+    }
+}
+
+$circ = new Circunferencia("Circunferencia",10);
+$circ->printCaracteristicas();
+
+$retangulo = new retangulo("Retangulo", 10, 20);
+$retangulo->printCaracteristicas();
